b952f0db888133a4da990492ef9e0f2678405453
Jonathan Modlin <jonathanmodlin1@gmail.com>
2020-07-05 04:09:11 -0400

    commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+function Header () {
+    return (
+        <div className="header">
+            <h1>Employee Directory</h1>
+            <p>Click on each heading to filter or use the search box</p>
+        </div>
+    )
+}
+
+export default Header;
+
+
