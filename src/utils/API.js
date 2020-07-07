64b5ad5a301aa001026dbd9948647ac5fb8e3d13
Jonathan Modlin <59783364+Agnys560@users.noreply.github.com>
2020-07-07 14:40:44 -0400

    Update API.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+//people generator
+
 export default{ 
     getUsers:function(){    
-
+        
         return axios.get("https://randomuser.me/api/?results=200&nat=us");
     }
 };
